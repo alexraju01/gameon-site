a8e370453edfc787b41dbc0af3f0b484d089b92b
alexraju01 <alex.raju1010@gmail.com>
2026-04-08 18:00:29 +0100

    update asset prefix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
 	output: "export", // enables static HTML export
 	basePath: isProd ? "/gameon-site" : "", // only if deploying to github.io/your-repo-name
-	assetPrefix: isProd ? "/gameon-site/" : "",
+	assetPrefix: isProd ? "/" : "",
 
 	images: {
 		unoptimized: true,
