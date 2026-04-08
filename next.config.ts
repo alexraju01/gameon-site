0e31be5763781d9f81711a1eb7e4b17cb6e6b36e
alexraju01 <alex.raju1010@gmail.com>
2026-04-08 16:56:15 +0100

    feat: update configuration for static export and add gh-pages support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+	output: "export", // enables static HTML export
+	basePath: "/gameon-site", // only if deploying to github.io/your-repo-name
+	assetPrefix: "/gameon-site/",
+
+	images: {
+		unoptimized: true,
+	},
+
+	/* config options here */
+	reactStrictMode: true,
 };
 
 export default nextConfig;
